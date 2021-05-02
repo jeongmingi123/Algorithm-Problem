9119f76ebc2708d23fdcef8ad68fafd3e59963d8
jeongmingi <mo4811@naver.com>
2021-05-03 00:59:24 +0900

    5/3 문제풀이 추가

---
+++ src/C#/baek10869/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace baek10869
+{
+    class Program
+    {
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        static int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        static int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        static int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        static int Remainder(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+        static void Main(string[] args)
+        {
+            String input = Console.ReadLine();
+            String[] inputAB = input.Split();
+
+            int A = int.Parse(inputAB[0]);
+            int B = int.Parse(inputAB[1]);
+
+            Console.WriteLine(Add(A, B));
+            Console.WriteLine(Sub(A, B));
+            Console.WriteLine(Mul(A, B));
+            Console.WriteLine(Div(A, B));
+            Console.WriteLine(Remainder(A, B));
+        }
+    }
+}
