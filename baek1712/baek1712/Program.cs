c664454d47d2a3b81b580e92226c203c69a7f586
jeongmingi <mo4811@naver.com>
2021-05-18 23:25:07 +0900

    5/18 손익분기점 문제 추가

---
+++ baek1712/baek1712/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace baek1712
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(" ");
+
+            int fixedCost = int.Parse(inputs[0]);
+            int variableCost = int.Parse(inputs[1]);
+            int notebookPrice = int.Parse(inputs[2]);
+
+
+            if (variableCost > notebookPrice)
+            {
+                Console.WriteLine("-1");
+            }
+
+            int breakEvenPoint = fixedCost / (notebookPrice - variableCost);
+
+            Console.WriteLine(breakEvenPoint + 1);
+
+
+
+
+
+        }
+    }
+}
