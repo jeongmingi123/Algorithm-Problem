247d6a1fd1d61079c0aa1a87fbc5486b35f3c7fb
jeongmingi <mo4811@naver.com>
2021-05-20 23:35:45 +0900

    백준 2869 알고리즘 문제 진행중

---
+++ src/C#/beak2869/beak2869/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+/*
+
+A = 낮에 올라갈 수있는 m 길이
+B = 미끄러지는 m 길이
+V = 높이(m)
+
+달팽이가 나무 막대를 모두 올라가려면, 며칠이 걸리는지 구하는 프로그램을 작성하시오.
+
+하루만에 올라가면 day = 1
+
+A = 2
+B = 1 
+V = 5
+
+4일 걸림
+
+1일째 2m 올라감 // 2
+2일째 1m 미끄러지고, 2m 올라감 // 2+1
+3일째 1m 미끄러지고, 2m 올라감 // 2+1+1
+4일째 1m 미끄러지고, 2m 올라감 // 2+1+1+1 << 높이
+
+ */
+
+
+
+namespace beak2869
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] inputs = Console.ReadLine().Split();
+            int climb = int.Parse(inputs[0]);
+            int slide = int.Parse(inputs[1]);
+            int mountainHeight = int.Parse(inputs[2]);
+
+            int currentHeight = climb;
+            int day = 1;
+
+            while (true)
+            {
+                if (currentHeight == mountainHeight)
+                {
+                    Console.WriteLine(day);
+                    break;
+                }
+                else
+                {
+                    currentHeight = currentHeight + climb - slide;
+                    day++;
+                }
+            }
+
+        }
+    }
+}
