b057f6fb2f0fa1f45c9f4119384d0e56902aaf3e
jeongmingi <mo4811@naver.com>
2021-07-04 12:50:54 +0900

    알고리즘 문제 추가

---
+++ src/C#/baek2439/baek2439/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace baek2439
+{
+    class Program
+    {
+
+        static void DrawReverseRightTriangle(int num)
+        {
+            for(int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= num; j++)
+                {
+                    if (i + j >= num + 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+             
+            }
+        }
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            DrawReverseRightTriangle(num);
+        }
+    }
+}
---
+++ src/C#/prog음양더하기/prog음양더하기/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prog음양더하기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
