3f6b1c24f5f2973e7f19e0bda475d0e4fad97a72
jeongmingi <mo4811@naver.com>
2021-07-05 23:19:29 +0900

    7/5 알고리즘 문제추가

---
+++ src/C#/codeup1929/codeup1929/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace codeup1929
+{
+    class Program
+    {
+        static void PrintReverseHails(int num)
+        {
+            if (num == 1)
+            {
+                
+                return;
+            }
+
+            if (num % 2 == 0)
+            {
+                PrintReverseHails(num / 2);
+                Console.WriteLine(num / 2);
+            }
+
+            if (num % 2 != 0)
+            {
+                PrintReverseHails(3 * num + 1);
+                Console.WriteLine(3 * num + 1);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            PrintReverseHails(num);
+            Console.WriteLine(num);
+        }
+    }
+}
