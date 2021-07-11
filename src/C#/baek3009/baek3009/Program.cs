7c1dc0af82beef66fd1cce36b7a4e8140a715b0a
jeongmingi <mo4811@naver.com>
2021-07-11 14:44:52 +0900

    [백준알고리즘] 3009번 문제

---
+++ src/C#/baek3009/baek3009/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace baek3009
+{
+    class Program
+    {
+        static void search4thPoint(int[] arr)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                int temp = arr[i];
+                int count = 0;
+
+                for(int j = 0; j < arr.Length; j++)
+                {
+                    if (temp == arr[j])
+                    {
+                        ++count;
+                    }
+                }
+
+                if (count == 1)
+                {
+                    arr[3] = temp;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] x = new int[4];
+            int[] y = new int[4];
+
+            for(int i = 0; i < 3; i++)
+            {
+                string[] line = Console.ReadLine().Split(" ");
+                x[i] = int.Parse(line[0]);
+                y[i] = int.Parse(line[1]);
+            }
+
+            search4thPoint(x);
+            search4thPoint(y);
+
+            Console.Write($"{x[3]} {y[3]}");
+
+
+        }
+    }
+}
