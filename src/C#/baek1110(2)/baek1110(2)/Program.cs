1bf53abbe9f9354eeea23dd776645332b1b49a50
jeongmingi <mo4811@naver.com>
2021-07-16 22:41:38 +0900

    [백준 1110번 (2) 문제 풀이]

---
+++ src/C#/baek1110(2)/baek1110(2)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace baek1110_2_
+{
+    class Program
+    {
+        static void getCycleToReturnOriginalNumberRecursive(int originalNum, int left, int right, int count)
+        {
+            int sum = left + right; // 8
+            int result = int.Parse(string.Format($"{right}{sum % 10}"));
+
+            if (result == originalNum)
+            {
+                Console.WriteLine(count);
+                return;
+            }
+
+            getCycleToReturnOriginalNumberRecursive(originalNum, right, sum % 10, count + 1); // 
+        }
+        static void Main(string[] args)
+        {
+            int origin = int.Parse(Console.ReadLine());
+            string temp = origin.ToString();
+
+            int num1 = int.Parse(temp) / 10;
+            int num2 = int.Parse(temp) % 10;
+
+            getCycleToReturnOriginalNumberRecursive(origin, num1, num2, 1);
+        }
+    }
+}
