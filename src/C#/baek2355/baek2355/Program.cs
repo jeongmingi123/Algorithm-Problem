ea66a60f65c2b66776ac9e173fa2c84f4e07c6e2
jeongmingi <mo4811@naver.com>
2021-07-18 22:34:12 +0900

    [백준 2355번 진행중]

---
+++ src/C#/baek2355/baek2355/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace baek2355
+{
+    class Program
+    {
+        static int GetSigmaValue(int start, int end)
+        {
+            if ((end - start + 1) % 2 == 0)
+            {
+                return (end + start) * (end / start) / 2;
+            }
+
+            return (end + start) * (end / start - 1) / 2 + (end + start) / 2;
+
+        }
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split();
+
+            int start = int.Parse(line[0]);
+
+            int end = int.Parse(line[1]);
+
+            int total = GetSigmaValue(start, end);
+            Console.WriteLine(total);
+        }
+    }
+}
