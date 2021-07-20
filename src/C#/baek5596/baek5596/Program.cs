f62f4870956642e60f54f46d663a6a0a3a9feb2f
jeongmingi <mo4811@naver.com>
2021-07-20 23:50:00 +0900

    [백준 5596번 문제 소스코드]

---
+++ src/C#/baek5596/baek5596/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace baek5596
+{
+    class Program
+    {
+        static int getTotalScore(int[] nums, int index)
+        {
+            if (index == nums.Length)
+            {
+                return 0;
+            }
+
+            return nums[index] + getTotalScore(nums, index + 1);
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] minGookScores = new int[4];
+            int[] jungboScores = new int[4];
+
+            for (int i = 0; i < 2; i++)
+            {
+                string[] lines = Console.ReadLine().Split();
+
+                if (i == 0)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        minGookScores[j] = int.Parse(lines[j]);
+                    }
+                }
+
+                else if (i == 1)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        jungboScores[j] = int.Parse(lines[j]);
+                    }
+                }
+
+            }
+
+            int minGookTotalScore = getTotalScore(minGookScores, 0);
+            int jungboTotalScore = getTotalScore(jungboScores, 0);
+
+            if (minGookTotalScore >= jungboTotalScore)
+            {
+                Console.WriteLine(minGookTotalScore);
+            }
+
+            else
+            {
+                Console.WriteLine(jungboTotalScore);
+            }
+
+        }
+    }
+}
