8613d868acc75c42a5f006225739ca860479b6ba
jeongmingi <mo4811@naver.com>
2021-07-22 22:59:45 +0900

    Update Program.cs

--- src/C#/baek1173/baek1173/Program.cs
+++ src/C#/baek1173/baek1173/Program.cs
@@ -4,7 +4,7 @@ namespace baek1173
 {
     class Program
     {
-        static int getExerciseTimeRecursive(int N, int X, int M, int T, int R, int minTime, int exerciseTime, int m)
+        static int getExerciseTimeRecursive(int N, int X, int M, int T, int R, int m, int minTime, int exerciseTime)
         {
             if (M - m < T)
             {
@@ -13,7 +13,7 @@ namespace baek1173
 
             else if (m > X)
             {
-                return getExerciseTimeRecursive(N, m, M, T, R, minTime, exerciseTime, m);
+                return getExerciseTimeRecursive(N, m, M, T, R, m, minTime, exerciseTime);
             }
 
             else if (exerciseTime == N)
@@ -24,12 +24,12 @@ namespace baek1173
 
             else if (X + T <= M)
             {
-                return getExerciseTimeRecursive(N, X + T, M, T, R, ++minTime, ++exerciseTime, m);
+                return getExerciseTimeRecursive(N, X + T, M, T, R, m, ++minTime, ++exerciseTime);
             }
 
             else if (X + T > M)
             {
-                return getExerciseTimeRecursive(N, X - R, M, T, R, ++minTime, exerciseTime, m);
+                return getExerciseTimeRecursive(N, X - R, M, T, R, m, ++minTime, exerciseTime);
             }
 
             return -1;
@@ -44,7 +44,7 @@ namespace baek1173
             int T = int.Parse(line[3]);
             int R = int.Parse(line[4]);
 
-            int time = getExerciseTimeRecursive(N, m, M, T, R, 0, 0, m);
+            int time = getExerciseTimeRecursive(N, m, M, T, R, m, 0, 0);
             Console.WriteLine(time);
         }
     }
