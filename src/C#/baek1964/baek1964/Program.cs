af427e5bed74bd53e3f5bbca450c122e1425aa1c
jeongmingi <mo4811@naver.com>
2021-07-23 16:44:45 +0900

    [백준 1964 문제 풀이 완료]

---
+++ src/C#/baek1964/baek1964/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace baek1964
+{
+    class Program
+    {
+        static long GetDotTotal(int N, int index)
+        {
+            if (N == 1)
+            {
+                return 5;
+            }
+
+            else if (index == 1)
+            {
+                return 5 + GetDotTotal(N, index + 1);
+            }
+
+            else if (index == N)
+            {
+                return index * 3 + 1;
+            }
+
+            return index * 3 + 1 + GetDotTotal(N, index + 1);
+        }
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetDotTotal(N, 1) % 45678);
+        }
+    }
+}
