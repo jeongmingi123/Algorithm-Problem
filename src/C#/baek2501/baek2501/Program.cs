cf45162ed12e61ae241d746bcf211483eef7d026
jeongmingi <mo4811@naver.com>
2021-07-27 13:03:37 +0900

    [백준 알고리즘 2501번 풀이]

---
+++ src/C#/baek2501/baek2501/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace baek2501
+{
+    class Program
+    {
+
+        static List<int> GetDivisors(int num)
+        {
+            List<int> divisors = new List<int>();
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    divisors.Add(i);
+                }
+            }
+
+            return divisors;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split();
+
+            int N = int.Parse(line[0]);
+            int K = int.Parse(line[1]);
+
+            List<int> divisors = GetDivisors(N);
+
+            if (divisors.Count < K)
+            {
+                Console.WriteLine("0");
+            }
+
+            else
+            {
+                Console.WriteLine(divisors[K-1]);
+            }
+
+        }
+    }
+}
