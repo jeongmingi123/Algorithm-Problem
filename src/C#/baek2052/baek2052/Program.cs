ae6443f27f700972fc0c4083d9401117e363a69c
jeongmingi <mo4811@naver.com>
2021-07-27 22:53:57 +0900

    [백준 2052번 문제 풀이 진행중]

---
+++ src/C#/baek2052/baek2052/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace baek2052
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Pow(2, -N));
+        }
+    }
+}
+
+/*
+ 
+N==1
+1/2
+
+N=2
+1/4
+
+N=3
+1/8
+
+N==4
+1/16
+
+N==5
+
+1/32
+
+
+
+
+
+
+ */
