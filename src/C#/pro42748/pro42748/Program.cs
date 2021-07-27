79fd7b12ef66635717a080735081c7e06cab169f
jeongmingi <mo4811@naver.com>
2021-07-28 00:28:18 +0900

    [프로그래머스 k번째 수 42478번 문제 풀이]

---
+++ src/C#/pro42748/pro42748/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pro42748
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 5, 2, 6, 3, 7, 4 };
+            int[,] commands = new int[,] { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
+
+            int[] result = Solution.solution(array, commands);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine($"{result[i]} ");
+            }
+        }
+    }
+}
---
+++ src/C#/pro42748/pro42748/Solution.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pro42748
+{
+    using System;
+
+    public class Solution
+    {
+        public static int GetNumberKth(int[] array, int start, int end, int k)
+        {
+            int length = end - start + 1;
+
+            int[] cuttedNums = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                cuttedNums[i] = array[start - 1];
+                ++start;
+            }
+
+            Array.Sort(cuttedNums);
+            return cuttedNums[k - 1];
+
+        }
+
+        public static int[] solution(int[] array, int[,] commands)
+        {
+
+            int[] answer = new int[commands.GetLength(0)];
+
+            int start = 0;
+            int end = 0;
+            int k = 0;
+
+            for (int i = 0; i < commands.GetLength(0); i++)
+            {
+                for (int j = 0; j < commands.GetLength(1); j++)
+                {
+                    if (j == 0)
+                    {
+                        start = commands[i, 0];
+                    }
+                    else if (j == 1)
+                    {
+                        end = commands[i, 1];
+                    }
+                    else if (j == 2)
+                    {
+                        k = commands[i, 2];
+                    }
+                }
+                answer[i] = GetNumberKth(array, start, end, k);
+            }
+
+            return answer;
+        }
+    }
+}
