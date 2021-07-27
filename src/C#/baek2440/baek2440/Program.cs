ea9e2d129953b1d73cdd1e41f73b8ed880950950
jeongmingi <mo4811@naver.com>
2021-07-27 12:51:02 +0900

    [백준알고리즘 2440문제 풀이]

---
+++ src/C#/baek2440/baek2440/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace baek2440
+{
+    class Program
+    {
+
+        static void PrintReverseRightTriangle(int num)
+        {
+            for (int i = num; i > 0; i--)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            PrintReverseRightTriangle(num);
+        }
+    }
+}
