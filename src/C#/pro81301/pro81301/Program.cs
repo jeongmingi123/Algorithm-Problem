6b74262fae809e2b619a7c91559f766ad92539ee
jeongmingi <mo4811@naver.com>
2021-07-29 21:32:04 +0900

    [프로그래머스 숫자 문자열과 영단어 81301 문제 풀이]

---
+++ src/C#/pro81301/pro81301/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pro81301
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/C#/pro81301/pro81301/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pro81301
+{
+    class Solution
+    {
+        public static int soultion(string s)
+        {
+            Dictionary<string, string> numsAndWords = new Dictionary<string, string>
+        { {"zero", "0"},
+          {"one", "1"},
+          {"two", "2"},
+          {"three", "3"},
+          {"four", "4"},
+          {"five", "5"},
+          {"six", "6"},
+          {"seven", "7"},
+          {"eight", "8"},
+          {"nine", "9"}
+        };
+
+            foreach (var keyValue in numsAndWords)
+            {
+                s = s.Replace(keyValue.Key, keyValue.Value);
+            }
+
+            int answer = int.Parse(s);
+            return answer;
+        }
+    }
+}
