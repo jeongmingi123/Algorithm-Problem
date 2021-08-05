690913d9f378ef6d88f21bf002030dca5652b3ff
jeongmingi <mo4811@naver.com>
2021-08-05 22:23:17 +0900

    [백준 알고리즘 2490 풀이 완료]

---
+++ src/C#/baek2490/baek2490/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+
+namespace baek2490
+{
+    class Program
+    {
+        static void PlayYut(StringBuilder sb1, int count)
+        {
+            if (count == 0)
+            {
+                sb1.AppendLine("E");
+            }
+
+            else if (count == 1)
+            {
+                sb1.AppendLine("A");
+            }
+
+            else if (count == 2)
+            {
+                sb1.AppendLine("B");
+            }
+
+            else if (count == 3)
+            {
+                sb1.AppendLine("C");
+            }
+
+            else if (count == 4)
+            {
+                sb1.AppendLine("D");
+            }
+        }
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 3; i++)
+            {
+                string line = Console.ReadLine();
+
+                string[] bars = line.Split(" ");
+
+                int count = 0;
+
+                for (int j = 0; j < 4; j++)
+                {
+                    if (int.Parse(bars[j]) == 0)
+                    {
+                        ++count;
+                    }
+                }
+
+                PlayYut(sb, count);
+
+            }
+
+            string result = sb.ToString();
+
+            Console.WriteLine(result);
+        }
+    }
+}
