1bff7b99c184bf042f4731b0b0a9dc0b0f5ec46e
jeongmingi <mo4811@naver.com>
2022-01-08 21:30:24 +0900

    commit

---
+++ src/C/baek14681/baek14681.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+	int num1;
+	int num2;
+	scanf("%d\n", &num1);
+	scanf("%d\n", &num2);
+
+
+	
+
+
+}
--- src/C/baek2588/baek2588.c
+++ src/C/baek2588/baek2588.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-	int num1;
-	int num2;
+	int a = 30;
 
-	int result1;
-	int result2;
-	int result3;
+	int* p;
+	p = &a;
 
-	int total;
+	printf("%p \n", (void*)&a);
 
-	scanf("%d", &num1);
-	scanf("%d", &num2);
-
-	result1 = num1 * (num2 % 10);
-	result2 = num1 * (num2 % 100 / 10);
-	result3 = num1 * (num2 / 100);
-
-	total = result1 + (result2 * 10) + (result3 * 100);
-
-	printf("%d\n", result1);
-	printf("%d\n", result2);
-	printf("%d\n", result3);
-	printf("%d\n", total);
-
+	printf("%p \n", (void*)p);
+	
 	return 0;
 }
