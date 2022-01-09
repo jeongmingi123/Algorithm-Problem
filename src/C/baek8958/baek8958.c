7332e84486658b9f841c6306946ae791f868afdd
jeongmingi <mo4811@naver.com>
2022-01-09 14:02:29 +0900

    commit

--- src/C/baek8958/baek8958.c
+++ src/C/baek8958/baek8958.c
@@ -11,24 +11,10 @@ int main(void) {
 	size_t i;
 
 	scanf_s("%d", &test_case);
-	
+
 	for (i = 0; i < test_case; i++) {
-		
 		while (fgets(str, LINE_LENGTH, stdin) != NULL) {
 			printf("%s\n", str);
 		}
-
 	}
-
-
-
 }
-
-
-/*
-
-1. 테스트 케이스 받기
-2. 줄별로 테스트 케이스에 대한 문자열 입력받기
-3. 
-
-*/
