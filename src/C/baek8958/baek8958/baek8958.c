47a3b5d30b3aa025d4dcc48326815fed7e1b2a2c
jeongmingi <mo4811@naver.com>
2022-01-11 22:50:13 +0900

    Update baek8958.c

--- src/C/baek8958/baek8958/baek8958.c
+++ src/C/baek8958/baek8958/baek8958.c
@@ -1,21 +1,35 @@
-#define LINE_LENGTH (128)
+#define LINE_LENGTH (256)
 #define TRUE (1)
 #include <stdio.h>
 
 int main(void) {
 	unsigned int test_case;
 	unsigned int idx = 0;
-	unsigned int i = 0;
 
-	char line[LINE_LENGTH];
+	char line[LINE_LENGTH][LINE_LENGTH];
 
-	fgets(line, LINE_LENGTH, stdin);
+	size_t i;
+
+	scanf_s("%u", &test_case);
+
+	for (i = 0; i < test_case; i++) {
+		fgets(line[i], LINE_LENGTH, stdin);
+		line[i][strlen(line[i]) - 1] = '\0';
+	}
+
+	for (i = 0; i < test_case; i++) {
+		printf("%s\n", line[i]);
+	}
+
+
 	/*
+	fgets(line, LINE_LENGTH, stdin);
+	
 	line[strlen(line) - 1] = '\0';
-	*/
+	
 	printf("%s\n", line);
 	printf("%d\n", strlen(line));
-
+	*/
 	return 0;
 }
 
