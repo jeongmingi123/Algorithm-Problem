ace2ebaee9a625f03411ce558fcbca6cef2588be
jeongmingi <mo4811@naver.com>
2022-01-13 00:22:07 +0900

    백준 11720번

---
+++ src/C/baek11720/baek11720/baek11720.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	char str[128] = { 0, };
+	unsigned int str_length;
+	int total = 0;
+	size_t i;
+
+	scanf_s("%u", &str_length);
+
+	scanf_s("%s", str, 128);
+
+	for (i = 0; i < str_length; i++) {
+		total += *(str + i) - 48;
+		
+	}
+
+	printf("%d\n", total);
+
+	return 0;
+}
